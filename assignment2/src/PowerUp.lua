2a48be68b35760c9ce311e969e45d3ce7053eddf
sshiva9001 <66074529+sshiva9001@users.noreply.github.com>
2020-06-16 21:20:13 +0800

    Add files via upload

---
+++ assignment2/src/PowerUp.lua
@@ -0,0 +1,84 @@
+
+
+PowerUp = Class{}
+
+function PowerUp:init(skin)
+
+	self.width = 16
+	self.height = 16
+
+	self.dx = 0
+	self.dy = 0 
+	self.x = 0
+	self.y = 0
+	self.timer = 0
+
+end
+
+
+
+
+function PowerUp:collides(target)
+    -- first, check to see if the left edge of either is farther to the right
+    -- than the right edge of the other
+    if self.x > target.x + target.width or target.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either is higher than the top
+    -- edge of the other
+    if self.y > target.y + target.height or target.y > self.y + self.height then
+        return false
+    end 
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
+function PowerUp:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dx = 0
+    self.dy = 0
+end
+
+
+
+
+
+function PowerUp:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+    self.timer = self.timer + dt
+
+    -- allow ball to bounce off walls
+    if self.x <= 0 then
+        self.x = 0
+        self.dx = -self.dx
+        gSounds['wall-hit']:play()
+    end
+
+    if self.x >= VIRTUAL_WIDTH - 8 then
+        self.x = VIRTUAL_WIDTH - 8
+        self.dx = -self.dx
+        gSounds['wall-hit']:play()
+    end
+
+    if self.y <= 0 then
+        self.y = 0
+        self.dy = -self.dy
+        gSounds['wall-hit']:play()
+    end
+
+end
+
+
+
+
+
+function PowerUp:render()
+    -- gTexture is our global texture for all blocks
+    -- gBallFrames is a table of quads mapping to each individual ball skin in the texture
+    love.graphics.draw(gTextures['main'], print(gFrames['powerups'][self.skin]), self.x, self.y)
+end
+
